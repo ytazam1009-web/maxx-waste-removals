b067d1a8c419f17adba2b14068c4a2bac5f44b68
absarh110 <absar_hussain@hotmail.com>
2026-08-26 15:48:09 +0500

    Add Meta Pixel tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Script from "next/script";
 import LocalBusinessSchema from "@/components/seo/LocalBusinessSchema";
 
+
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -134,6 +135,28 @@ export default function RootLayout({ children }) {
             });
           `}
         </Script>
+        
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '934465092422525');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
 
         <LocalBusinessSchema />
         {children}
